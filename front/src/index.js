0602fb6406fa073538b852e8b302cfaa9f3f32ea
Sergey Gerenko <ailus665@gmail.com>
2021-12-26 14:29:00 +0200

    Added scrollbars styling. Minor fixes.

--- front/src/index.js
+++ front/src/index.js
@@ -1,37 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {ThemeProvider} from "@emotion/react";
+import {createTheme} from "@mui/material";
 import reportWebVitals from './reportWebVitals';
 import App from './App';
 import './index.css';
-import {ThemeProvider} from "@emotion/react";
-import {createTheme} from "@mui/material";
 
 /* Select Theme by User pereferences */
-export const light = {
+const light = {
     palette: {
         mode: 'light',
     },
 }
-export const dark = {
+const dark = {
     palette: {
         mode: 'dark',
     },
 }
-
 let isUserThemeDark = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
-
-console.log('isDark = ' + isUserThemeDark);
-
 const appliedTheme = createTheme(isUserThemeDark ? dark : light);
 /* Select Theme by User pereferences */
 
 ReactDOM.render(
-  <React.StrictMode>
-      <ThemeProvider theme={appliedTheme}>
-          <App />
-      </ThemeProvider>
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <ThemeProvider theme={appliedTheme}>
+            <App/>
+        </ThemeProvider>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
