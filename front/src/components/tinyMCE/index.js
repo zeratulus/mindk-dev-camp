765c5f448838a9ddc3fedb9b93508c727e3185c9
Sergey Gerenko <ailus665@gmail.com>
2022-02-03 16:36:54 +0200

    FE: Post Form and Profile Form with Formik. Added cropper.

--- front/src/components/tinyMCE/index.js
+++ front/src/components/tinyMCE/index.js
@@ -1,25 +1,37 @@
+import {Field} from 'formik';
 import {Editor} from "@tinymce/tinymce-react";
 import React from "react";
 
-export function TinyMCE({editorRef, value}) {
-    return (<Editor
-        onInit={(evt, editor) => editorRef.current = editor}
-        initialValue={value}
-        init={{
-            height: 360,
-            menubar: false,
-            plugins: [
-                'advlist autolink lists link charmap print preview anchor paste',
-                'searchreplace visualblocks code fullscreen',
-                'insertdatetime media table paste code help wordcount emoticons image imagetools'
-            ],
-            toolbar: 'undo redo | paste | formatselect | ' +
-                'bold italic backcolor | alignleft aligncenter ' +
-                'alignright alignjustify | bullist numlist outdent indent | ' +
-                'removeformat | image | emoticons | help',
-            content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }',
-            skin: (window.matchMedia("(prefers-color-scheme: dark)").matches ? "oxide-dark" : ""),
-            content_css: (window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : ""),
-            paste_data_images: true
-        }} />);
+export function TinyMCE({editorRef, name}) {
+    return (
+        <Field name={name} id={name} type="text">
+            {({field: {value}, form: {setFieldValue}}) => (
+                <Editor
+                    onInit={(evt, editor) => editorRef.current = editor}
+                    initialValue={value}
+                    onEditorChange={(e) => {
+                        setFieldValue(name, e);
+                        console.log('onEditorChange>>>', name, e)
+                    }}
+                    init={{
+                        height: 360,
+                        menubar: false,
+                        plugins: [
+                            'advlist autolink lists link charmap print preview anchor paste',
+                            'searchreplace visualblocks code fullscreen',
+                            'insertdatetime media table paste code help wordcount emoticons image imagetools'
+                        ],
+                        toolbar: 'undo redo | paste | formatselect | ' +
+                            'bold italic backcolor | alignleft aligncenter ' +
+                            'alignright alignjustify | bullist numlist outdent indent | ' +
+                            'removeformat | image | emoticons | help',
+                        content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }',
+                        skin: (window.matchMedia("(prefers-color-scheme: dark)").matches ? "oxide-dark" : ""),
+                        content_css: (window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : ""),
+                        paste_data_images: true
+                    }}/>
+            )
+            }
+        </Field>
+    );
 }
