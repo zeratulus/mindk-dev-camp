4e3e597dd67231622105050e1c15ccfbecb99c7f
Sergey Gerenko <ailus665@gmail.com>
2022-02-09 00:33:00 +0200

    BE: async error handler.

--- api/src/utils/index.js
+++ api/src/utils/index.js
@@ -1,6 +1,10 @@
 const url = require("url");
 const config = require("../services/config");
 
+const asyncLogHandler = (fn) => (req, res, next) => {
+    return Promise.resolve(fn(req, res, next)).then(next).catch((e) => next(e));
+}
+
 function log(msg) {
     console.log(`${new Date().toISOString()} -> ${msg}`);
 }
